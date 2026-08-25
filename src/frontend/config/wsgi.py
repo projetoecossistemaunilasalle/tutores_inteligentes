8ff115d54dbdf11327e1f6138ecdf6f658b9f44a
Marcos-trindade <marqtrind@gmail.com>
2026-08-25 10:51:58 -0300

    chore: remove residuos da arquitetura antiga (manage_frontend, config do frontend, backups)

--- src/frontend/config/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""WSGI do frontend (Grupo 2)."""
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "frontend.config.settings"
-)
-application = get_wsgi_application()
